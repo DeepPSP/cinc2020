c0c20acdd69a95ea22a57ab3a44e17b347684983
WEN Hao <wenh06@gmail.com>
2020-07-20 09:39:56 +0800

    add several smoothing methods

--- utils/misc.py
+++ utils/misc.py
@@ -2,12 +2,17 @@
 """
 import os
 from copy import deepcopy
-from typing import Union, Optional, List
+from typing import Union, Optional, List, Sequence
+
+import numpy as np
 
 
 __all__ = [
     "get_record_list_recursive",
     "dict_to_str",
+    "diff_with_step",
+    "smooth",
+    "MovingAverage",
 ]
 
 
@@ -93,3 +98,233 @@ def dict_to_str(d:Union[dict, list, tuple], current_depth:int=1, indent_spaces:i
     s += unit_indent*(current_depth-1)
     s = f"{{{s}}}" if isinstance(d, dict) else f"[{s}]"
     return s
+
+
+def diff_with_step(a:np.ndarray, step:int=1, **kwargs) -> np.ndarray:
+    """ finished, checked,
+
+    compute a[n+step] - a[n] for all valid n
+
+    Parameters:
+    -----------
+    a: ndarray,
+        the input data
+    step: int, default 1,
+        the step to compute the difference
+    kwargs: dict,
+
+    Returns:
+    --------
+    d: ndarray:
+        the difference array
+    """
+    if step >= len(a):
+        raise ValueError(f"step ({step}) should be less than the length ({len(a)}) of `a`")
+    d = a[step:] - a[:-step]
+    return d
+
+
+def smooth(x:np.ndarray, window_len:int=11, window:str='hanning', mode:str='valid', keep_dtype:bool=True) -> np.ndarray:
+    """ finished, checked
+    
+    smooth the 1d data using a window with requested size.
+    
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal 
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+    
+    Parameters:
+    -----------
+    x: ndarray,
+        the input signal 
+    window_len: int, default 11,
+        the length of the smoothing window,
+        (previously should be an odd integer, currently can be any (positive) integer)
+    window: str, default 'hanning',
+        the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman',
+        flat window will produce a moving average smoothing
+    mode: str, default 'valid',
+        ref. np.convolve
+    keep_dtype: bool, default True,
+        dtype of the returned value keeps the same with that of `x` or not
+
+    Returns:
+    --------
+    y: ndarray,
+        the smoothed signal
+        
+    Example:
+    --------
+    >>> t = linspace(-2, 2, 0.1)
+    >>> x = sin(t) + randn(len(t)) * 0.1
+    >>> y = smooth(x)
+    
+    See also: 
+    ---------
+    np.hanning, np.hamming, np.bartlett, np.blackman, np.convolve
+    scipy.signal.lfilter
+ 
+    TODO: the window parameter could be the window itself if an array instead of a string
+
+    NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
+
+    References:
+    -----------
+    [1] https://scipy-cookbook.readthedocs.io/items/SignalSmooth.html
+    """
+    radius = min(len(x), window_len)
+    radius = radius if radius%2 == 1 else radius-1
+
+    if x.ndim != 1:
+        raise ValueError("smooth only accepts 1 dimension arrays.")
+
+    # if x.size < radius:
+    #     raise ValueError("Input vector needs to be bigger than window size.")
+
+    if radius < 3:
+        return x
+    
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError("Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'")
+
+    s = np.r_[x[radius-1:0:-1], x, x[-2:-radius-1:-1]]
+    #print(len(s))
+    if window == 'flat': #moving average
+        w = np.ones(radius,'d')
+    else:
+        w = eval('np.'+window+'(radius)')
+
+    y = np.convolve(w/w.sum(), s, mode=mode)
+    y = y[(radius//2-1):-(radius//2)-1]
+    assert len(x) == len(y)
+
+    if keep_dtype:
+        y = y.astype(x.dtype)
+    
+    return y
+
+
+class MovingAverage(object):
+    """
+
+    moving average
+
+    References:
+    -----------
+    [1] https://en.wikipedia.org/wiki/Moving_average
+    """
+    def __init__(self, data:Sequence, **kwargs):
+        """
+        Parameters:
+        -----------
+        data: sequence,
+            the series data to compute its moving average
+        """
+        self.data = np.array(data)
+        self.verbose = kwargs.get("verbose", 0)
+
+    def cal(self, method:str, **kwargs) -> np.ndarray:
+        """
+        Parameters:
+        -----------
+        method: str,
+            method for computing moving average, can be one of
+            - 'sma', 'simple', 'simple moving average'
+            - 'ema', 'ewma', 'exponential', 'exponential weighted', 'exponential moving average', 'exponential weighted moving average'
+            - 'cma', 'cumulative', 'cumulative moving average'
+            - 'wma', 'weighted', 'weighted moving average'
+        """
+        m = method.lower().replace('_', ' ')
+        if m in ['sma', 'simple', 'simple moving average']:
+            func = self._sma
+        elif m in ['ema', 'ewma', 'exponential', 'exponential weighted', 'exponential moving average', 'exponential weighted moving average']:
+            func = self._ema
+        elif m in ['cma', 'cumulative', 'cumulative moving average']:
+            func = self._cma
+        elif m in ['wma', 'weighted', 'weighted moving average']:
+            func = self._wma
+        else:
+            raise NotImplementedError
+        return func(**kwargs)
+
+    def _sma(self, window:int=5, center:bool=False, **kwargs) -> np.ndarray:
+        """
+        simple moving average
+
+        Parameters:
+        -----------
+        window: int, default 5,
+            window length of the moving average
+        center: bool, default False,
+            if True, when computing the output value at each point, the window will be centered at that point;
+            otherwise the previous `window` points of the current point will be used
+        """
+        smoothed = []
+        if center:
+            hw = window//2
+            window = hw*2+1
+        for n in range(window):
+            smoothed.append(np.mean(self.data[:n+1]))
+        prev = smoothed[-1]
+        for n, d in enumerate(self.data[window:]):
+            s = prev + (d - self.data[n]) / window
+            prev = s
+            smoothed.append(s)
+        smoothed = np.array(smoothed)
+        if center:
+            smoothed[hw:-hw] = smoothed[window-1:]
+            for n in range(hw):
+                smoothed[n] = np.mean(self.data[:n+hw+1])
+                smoothed[-n-1] = np.mean(self.data[-n-hw-1:])
+        return smoothed
+
+    def _ema(self, weight:float=0.6, **kwargs) -> np.ndarray:
+        """
+        exponential moving average,
+        which is also the function used in Tensorboard Scalar panel,
+        whose parameter `smoothing` is the `weight` here
+
+        Parameters:
+        -----------
+        weight: float, default 0.6,
+            weight of the previous data point
+        """
+        smoothed = []
+        prev = self.data[0]
+        for d in self.data:
+            s = prev * weight + (1 - weight) * d
+            prev = s
+            smoothed.append(s)
+        smoothed = np.array(smoothed)
+        return smoothed
+
+    def _cma(self, **kwargs) -> np.ndarray:
+        """
+        cumulative moving average
+        """
+        smoothed = []
+        prev = 0
+        for n, d in enumerate(self.data):
+            s = prev + (d - prev) / (n+1)
+            prev = s
+            smoothed.append(s)
+        smoothed = np.array(smoothed)
+        return smoothed
+
+    def _wma(self, window:int=5, **kwargs) -> np.ndarray:
+        """
+        weighted moving average
+
+        Parameters:
+        -----------
+        window: int, default 5,
+            window length of the moving average
+        """
+        # smoothed = []
+        # total = []
+        # numerator = []
+        conv = np.arange(1, window+1)[::-1]
+        deno = np.sum(conv)
+        smoothed = np.convolve(conv, self.data, mode='same') / deno
+        return smoothed
