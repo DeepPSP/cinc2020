ca0c8093b41bfc108d18095641bfa23dc23df018
WEN Hao <wenh06@gmail.com>
2020-07-26 15:18:13 +0800

    update info about references

---
+++ references/__init__.py
@@ -0,0 +1,12 @@
+"""
+references,
+mainly for model development
+
+[1] Yao, Qihang, et al. "Multi-class Arrhythmia detection from 12-lead varied-length ECG using Attention-based Time-Incremental Convolutional Neural Network." Information Fusion 53 (2020): 174-182.
+[2] Hannun, Awni Y., et al. "Cardiologist-level arrhythmia detection and classification in ambulatory electrocardiograms using a deep neural network." Nature medicine 25.1 (2019): 65.
+[3] https://stanfordmlgroup.github.io/projects/ecg2/
+[4] https://github.com/awni/ecg
+[5] Hong, Shenda, et al. "ENCASE: An ENsemble ClASsifiEr for ECG classification using expert features and deep neural networks." 2017 Computing in cardiology (cinc). IEEE, 2017.
+
+to add more...
+"""
