ccdc507579468c58acd4c0cb55e68f0cded7aeb0
WEN Hao <wenh06@gmail.com>
2020-07-26 16:20:02 +0800

    add reference

--- references/__init__.py
+++ references/__init__.py
@@ -7,6 +7,8 @@ mainly for model development
 [3] https://stanfordmlgroup.github.io/projects/ecg2/
 [4] https://github.com/awni/ecg
 [5] Hong, Shenda, et al. "ENCASE: An ENsemble ClASsifiEr for ECG classification using expert features and deep neural networks." 2017 Computing in cardiology (cinc). IEEE, 2017.
+[6] Ilse, Maximilian, Jakub M. Tomczak, and Max Welling. "Attention-based deep multiple instance learning." arXiv preprint arXiv:1802.04712 (2018).
+[7] https://github.com/AMLab-Amsterdam/AttentionDeepMIL
 
 to add more...
 """
