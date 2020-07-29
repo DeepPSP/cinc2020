b43fdd3c1bf02a76663ab9c39b77f521dd8b9795
WEN Hao <wenh06@gmail.com>
2020-07-29 14:03:33 +0800

    update status about wave delineation using cwt

--- signal_processing/ecg_waves_cwt.py
+++ signal_processing/ecg_waves_cwt.py
@@ -2,12 +2,23 @@
 
 12-lead ECG wave delineation, using algorithms proposed in ref. [1]
 
+Update:
+no existing implementation of daubechies cwt, hence time consuming.
+priority of implementation of this algorithm is set LOW
+
 References:
 -----------
 [1] Yochum, Maxime, Charlotte Renaud, and Sabir Jacquir. "Automatic detection of P, QRS and T patterns in 12 leads ECG signal based on CWT." Biomedical Signal Processing and Control 25 (2016): 46-52.
+[2] Li, Cuiwei, Chongxun Zheng, and Changfeng Tai. "Detection of ECG characteristic points using wavelet transforms." IEEE Transactions on biomedical Engineering 42.1 (1995): 21-28.
+[3] https://encyclopediaofmath.org/wiki/Daubechies_wavelets
 """
 from typing import Union, Optional, NoReturn
 
 import numpy as np
 from pywt import cwt
 
+
+def continuous_daubechies():
+    """
+    """
+    raise NotImplementedError
