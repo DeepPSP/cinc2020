f34cc7b148a7ba2eaad835ce134af4368eb633ff
WEN Hao <wenh06@gmail.com>
2020-08-03 09:34:37 +0800

    debug spectral_heart_rate

--- signal_processing/ecg_spectral.py
+++ signal_processing/ecg_spectral.py
@@ -3,7 +3,7 @@ spectral analysis on the sequence of rr intervals (hrv, etc.),
 and on the ecg signal itself (heart rate, etc.)
 """
 from numbers import Real
-from typing import Union, Optional, NoReturn
+from typing import Union, Optional, Sequence, NoReturn
 
 import numpy as np
 np.set_printoptions(precision=5, suppress=True)
@@ -54,29 +54,38 @@ def spectral_heart_rate(filtered_sig:np.ndarray, fs:Real, hr_fs_band:Optional[Se
     else:
         s = filtered_sig.copy()
     
-    # psd of shape (c,n), freqs of shape (n,)
-    # where n = length of signal, c = number of leads
+    # psd of shape (c,n,k), freqs of shape (n,)
+    # where n = length of signal, c = number of leads, k rel. to freq bands
     freqs, _, psd = SS.spectrogram(s, fs, axis=-1)
     fs_band = hr_fs_band or FeatureCfg.spectral_hr_fs_band
     assert len(fs_band) >= 2, "frequency band of heart rate should at least has 2 bounds"
     fs_band = sorted(fs_band)
     fs_band = [fs_band[0], fs_band[-1]]
 
-    inds_of_interest = np.where((fs_band[0] <= freqs) & (freqs <= fs_band[1]))[0]
+    if verbose >= 1:
+        print(f"signal shape = {s.shape}")
+        print(f"fs_band = {fs_band}")
+        print(f"freqs.shape = {freqs.shape}, psd.shape = {psd.shape}")
+        print(f"freqs = {freqs.tolist()}")
+
+    inds_of_interest = np.where((fs_band[0] <= freqs) & (freqs <= fs_band[-1]))[0]
     # psd_of_interest of shape (c, m), freqs_of_interest of shape (m,)
     # where m = length of inds_of_interest
     freqs_of_interest = freqs[inds_of_interest]
-    psd_of_interest = psd[...,inds_of_interest]
-    peak_inds = np.argmax(psd_of_interest, axis=-1).flatten()
+    psd_of_interest = psd[:,inds_of_interest,:]
+    peak_inds = np.argmax(psd_of_interest, axis=-1)
 
     if verbose >= 1:
+        print(f"inds_of_interest = {inds_of_interest.tolist()}")
         print(f"freqs_of_interest = {freqs_of_interest.tolist()}")
-        print(f"peak_inds = {peak_inds.tolist()}")
+        print(f"peak_inds.shape = {peak_inds.shape}, peak_inds = {peak_inds.tolist()}")
+        print(f"psd_of_interest.shape = {psd_of_interest.shape}")
 
     # averaging at a neighborhood of `peak_idx`
     n_nbh = 1
     psd_mask = np.zeros_like(psd_of_interest, dtype=int)
     for l in range(psd_mask.shape[0]):
+        # TODO: fix bug here
         psd_mask[l, max(0,peak_inds[l]-n_nbh):min(psd_mask.shape[-1],peak_inds[l]+n_nbh)] = 1
     psd_of_interest = psd_of_interest * psd_mask
     # ret_val with units in second^{-1}
