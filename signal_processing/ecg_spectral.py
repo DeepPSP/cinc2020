055b1504eb8ec88bfa4bcc49fec3c09e69393205
WEN Hao <wenh06@gmail.com>
2020-08-03 09:14:06 +0800

    finish spectral_heart_rate, to do testing

--- signal_processing/ecg_spectral.py
+++ signal_processing/ecg_spectral.py
@@ -18,7 +18,7 @@ __all__ = [
 ]
 
 
-def spectral_heart_rate(filtered_sig:np.ndarray, fs:Real, hr_fs_band:Optional[Sequence[Real]]=None, sig_fmt:str="channel_first", verbose:int=0) -> Real:
+def spectral_heart_rate(filtered_sig:np.ndarray, fs:Real, hr_fs_band:Optional[Sequence[Real]]=None, sig_fmt:str="channel_first", mode:str='hr', verbose:int=0) -> Real:
     """ finished, NOT checked,
 
     compute heart rate of a ecg signal using spectral method (from the frequency domain)
@@ -35,13 +35,18 @@ def spectral_heart_rate(filtered_sig:np.ndarray, fs:Real, hr_fs_band:Optional[Se
         format of the multi-lead ecg signal,
         'channel_last' (alias 'lead_last'), or
         'channel_first' (alias 'lead_first', original)
+    mode: str, default 'hr',
+        mode of computation (return mean heart rate or mean rr intervals),
+        can also be 'heart_rate' (alias of 'hr'), and 'rr' (with an alias of 'rr_interval'),
+        case insensitive
     verbose: int, default 0,
         print verbosity
     
     Returns:
     --------
-    hr: real number,
-        heart rate of the ecg signal, with units in bpm
+    ret_val: real number,
+        mean heart rate of the ecg signal, with units in bpm;
+        or mean rr intervals, with units in ms
     """
     assert sig_fmt.lower() in ['channel_first', 'lead_first', 'channel_last', 'lead_last']
     if sig_fmt.lower() in ['channel_last', 'lead_last']:
@@ -49,6 +54,8 @@ def spectral_heart_rate(filtered_sig:np.ndarray, fs:Real, hr_fs_band:Optional[Se
     else:
         s = filtered_sig.copy()
     
+    # psd of shape (c,n), freqs of shape (n,)
+    # where n = length of signal, c = number of leads
     freqs, _, psd = SS.spectrogram(s, fs, axis=-1)
     fs_band = hr_fs_band or FeatureCfg.spectral_hr_fs_band
     assert len(fs_band) >= 2, "frequency band of heart rate should at least has 2 bounds"
@@ -56,12 +63,26 @@ def spectral_heart_rate(filtered_sig:np.ndarray, fs:Real, hr_fs_band:Optional[Se
     fs_band = [fs_band[0], fs_band[-1]]
 
     inds_of_interest = np.where((fs_band[0] <= freqs) & (freqs <= fs_band[1]))[0]
+    # psd_of_interest of shape (c, m), freqs_of_interest of shape (m,)
+    # where m = length of inds_of_interest
     freqs_of_interest = freqs[inds_of_interest]
     psd_of_interest = psd[...,inds_of_interest]
-    peak_inds = np.argmax(psd_of_interest, axis=-1)
+    peak_inds = np.argmax(psd_of_interest, axis=-1).flatten()
+
+    if verbose >= 1:
+        print(f"freqs_of_interest = {freqs_of_interest.tolist()}")
+        print(f"peak_inds = {peak_inds.tolist()}")
 
     # averaging at a neighborhood of `peak_idx`
-    # TODO: ajust for multi-channel cases
-    # psd_of_interest[:max(0,peak_idx-1)] = 0
-    # psd_of_interest[peak_idx+2:] = 0
-    # hr = 60 * np.dot(freqs_of_interest, psd_of_interest) / np.sum(psd_of_interest)
+    n_nbh = 1
+    psd_mask = np.zeros_like(psd_of_interest, dtype=int)
+    for l in range(psd_mask.shape[0]):
+        psd_mask[l, max(0,peak_inds[l]-n_nbh):min(psd_mask.shape[-1],peak_inds[l]+n_nbh)] = 1
+    psd_of_interest = psd_of_interest * psd_mask
+    # ret_val with units in second^{-1}
+    ret_val = np.mean(np.dot(psd_of_interest, freqs_of_interest) / np.sum(psd_of_interest, axis=-1))
+    if mode.lower() in ['hr', 'heart_rate']:
+        ret_val = 60 * ret_val
+    elif mode.lower() in ['rr', 'rr_interval']:
+        ret_val = 1000 / ret_val
+    return ret_val
