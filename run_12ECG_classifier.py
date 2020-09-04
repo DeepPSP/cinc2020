e75c495dfded9dd6fc5bb92f76cc62e67ce69ae0
WEN Hao <wenh06@gmail.com>
2020-09-04 14:42:11 +0800

    update the entrance for making final predictions from special detectors and deep models

--- run_12ECG_classifier.py
+++ run_12ECG_classifier.py
@@ -2,37 +2,60 @@
 
 import os, sys
 import joblib
-from io import StringIO
+from copy import deepcopy
+from typing import Dict, Tuple, List
 
 import numpy as np
 import wfdb
 import torch
+from torch import nn
 from scipy.signal import resample, resample_poly
-
-torch.set_default_tensor_type(torch.DoubleTensor)
 
 from get_12ECG_features import get_12ECG_features
 from models.special_detectors import special_detectors
-from utils.misc import rdheader, ensure_lead_fmt
+from models.ecg_crnn import ECG_CRNN
+from model_configs.ecg_crnn import ECG_CRNN_CONFIG
+from utils.misc import rdheader, ensure_lead_fmt, extend_predictions
+from utils.utils_signal import butter_bandpass_filter
 from cfg import ModelCfg, TrainCfg
 
+if ModelCfg.torch_dtype.lower() == "double"
+    torch.set_default_tensor_type(torch.DoubleTensor)
 
-def run_12ECG_classifier(data, header_data, loaded_model):
+
+def run_12ECG_classifier(data:np.ndarray, header_data:List[str], loaded_model:Dict[str, nn.Module], verbose:int=0) -> Tuple[List[int], List[float], List[str]]:
     """
     """
-    classes = TrainCfg.classes + ['Brady', 'LAD', 'RAD', 'PR', 'LQRSV']
+    dtype = np.float32 if ModelCfg.torch_dtype == "float" else np.float64
+
     header = rdheader(header_data)
-    data = ensure_lead_fmt(data, fmt="lead_first")
-    baseline = np.array(header.baseline).reshape(data.shape[0], -1)
-    adc_gain = np.array(header.adc_gain).reshape(data.shape[0], -1)
-    data = (data - baseline) / adc_gain
+    raw_data = ensure_lead_fmt(data.copy(), fmt="lead_first")
+    baseline = np.array(header.baseline).reshape(raw_data.shape[0], -1)
+    adc_gain = np.array(header.adc_gain).reshape(raw_data.shape[0], -1)
+    raw_data = (raw_data - baseline) / adc_gain
 
     freq = header.fs
     if freq != ModelCfg.freq:
-        data = resample_poly(data, ModelCfg.freq, freq, axis=1)
+        raw_data = resample_poly(raw_data, ModelCfg.freq, freq, axis=1)
         freq = ModelCfg.freq
 
-    partial_conclusion = special_detectors(data, freq, sig_fmt="lead_first")
+    dl_data = raw_data.copy()
+    if TrainCfg.bandpass is not None:
+        # bandpass
+        dl_data = butter_bandpass_filter(
+            dl_data,
+            lowcut=TrainCfg.bandpass[0],
+            highcut=TrainCfg.bandpass[1],
+            order=5,
+            fs=TrainCfg.fs,
+        )
+    if TrainCfg.normalize_data:
+        # normalize
+        dl_data = ((data - np.mean(dl_data)) / np.std(dl_data)).astype(dtype)
+
+    final_scores, final_conclusions = [], []
+
+    partial_conclusion = special_detectors(raw_data, freq, sig_fmt="lead_first")
     is_brady = partial_conclusion.is_brady
     is_tachy = partial_conclusion.is_tachy
     is_LAD = partial_conclusion.is_LAD
@@ -40,38 +63,100 @@ def run_12ECG_classifier(data, header_data, loaded_model):
     is_PR = partial_conclusion.is_PR
     is_LQRSV = partial_conclusion.is_LQRSV
 
-    tmp = np.zeros(shape=(len(classes,)))
-    tmp[classes.index('Brady')] = int(is_brady)
-    tmp[classes.index('LAD')] = int(is_LAD)
-    tmp[classes.index('RAD')] = int(is_RAD)
-    tmp[classes.index('PR')] = int(is_PR)
-    tmp[classes.index('LQRSV')] = int(is_LQRSV)
+    tmp = np.zeros(shape=(len(ModelCfg.full_classes,)))
+    tmp[ModelCfg.full_classes.index('Brady')] = int(is_brady)
+    tmp[ModelCfg.full_classes.index('LAD')] = int(is_LAD)
+    tmp[ModelCfg.full_classes.index('RAD')] = int(is_RAD)
+    tmp[ModelCfg.full_classes.index('PR')] = int(is_PR)
+    tmp[ModelCfg.full_classes.index('LQRSV')] = int(is_LQRSV)
     partial_conclusion = tmp
 
-    # normalize
-    normalized_data = ((data - np.mean(data)) / np.std(data)).astype(np.float32)
+    final_scores.append(partial_conclusion)
+    final_conclusions.append(partial_conclusion)
     
     # TODO: DL models to make decisions
+    dl_scores = []
+    for subset, model in loaded_model.items():
+        subset_scores, subset_bin = model.inference(torch.from_numpy(normalized_data))
+        # subset_scores = extend_predictions(
+        #     subset_scores,
+        #     TrainCfg.tranche_classes[subset],
+        #     ModelCfg.full_classes,
+        # )
+        # subset_bin = extend_predictions(
+        #     subset_bin,
+        #     TrainCfg.tranche_classes[subset],
+        #     ModelCfg.full_classes,
+        # )
+        # scores.append(subset_scores)
+        # conclusions.append(subset_bin)
+        subset_scores = extend_predictions(
+            subset_scores,
+            ModelCfg.tranche_classes[subset],
+            ModelCfg.dl_classes,
+        )
+        dl_scores.append(subset_scores)
 
-    current_score, _ = \
-        loaded_model.inference(torch.from_numpy(normalized_data))
+    if "NSR" in ModelCfg.dl_classes:
+        dl_nsr_cid = ModelCfg.dl_classes.index("NSR")
+    elif "426783006" in ModelCfg.dl_classes:
+        dl_nsr_cid = ModelCfg.dl_classes.index("426783006")
+    else:
+        dl_nsr_cid = None
+    # TODO: make a classifier using the scores from the 4 different dl models
+    dl_scores = np.max(np.array(dl_scores), axis=0)
 
-    # merge results from DL models with special detectors
-    # in a 'max pooling' manner
-    current_score = np.max(np.array([partial_conclusion, scalar_pred]), axis=0)
+    dl_conclusions = (dl_scores >= ModelCfg.bin_pred_thr).astype(int)
+    if dl_scores.min() < ModelCfg.bin_pred_nsr_thr and dl_nsr_cid is not None:
+        dl_conclusions[row_idx, dl_nsr_cid] = 1
+    elif dl_conclusions.sum() == 0:
+        max_val = dl_scores.max()
+        dl_conclusions = ((dl_scores+ModelCfg.bin_pred_look_again_tol) >= max_val)
+        dl_conclusions = dl_conclusions & (dl_scores >= ModelCfg.bin_pred_nsr_thr)
+        dl_conclusions = dl_conclusions.astype(int)
 
-    current_label = np.where(scalar_pred >= TrainCfg.bin_pred_thr)[0].astype(int).tolist()
-    if sum(current_label) == 0:
-        current_label[np.argmax(current_score)] = 1
+    dl_scores = extend_predictions(
+        dl_scores,
+        ModelCfg.dl_classes,
+        ModelCfg.full_classes,
+    )
+    dl_conclusions = extend_predictions(
+        dl_conclusions,
+        ModelCfg.dl_classes,
+        ModelCfg.full_classes,
+    )
+
+    final_scores.append(dl_scores)
+    final_conclusions.append(dl_conclusions)
+    final_scores = np.max(final_scores, axis=0)
+    final_conclusions = np.max(final_conclusions, axis=0)
+
+    current_label = final_conclusions
+    current_score = final_scores
+    classes = ModelCfg.full_classes
 
     return current_label, current_score, classes
 
 
 def load_12ECG_model(input_directory):
-    # # load the model from disk 
-    # f_out='finalized_model.sav'
-    # filename = os.path.join(input_directory,f_out)
+    """
+    """
+    loaded_model = ED()
+    
+    for k in ["AB", "E", "F"]:
+        model_config = deepcopy(ECG_CRNN_CONFIG)
+        classes = ModelCfg.tranche_classes[k]
+        loaded_model[k] = ECG_CRNN(
+            classes=classes,
+            config=model_config,
+        )
+        model_weight_path = ModelCfg.tranche_model[k]
+        loaded_model[k].load_state_dict(torch.load(model_weight_path))
 
-    # loaded_model = joblib.load(filename)
+    loaded_model["all"] = ECG_CRNN(
+        classes=ModelCfg.dl_classes,
+        config=deepcopy(ECG_CRNN_CONFIG),
+    )
+    loaded_model["all"].load_state_dict(torch.load(ModelCfg.tranche_model["all"]))
 
     return loaded_model
